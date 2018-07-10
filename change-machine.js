69ba9fe2596419a635133ddd915c0252d51d43ae
Kevin Chiriboga <kevin.chiriboga102@gmail.com>
2018-07-10 12:41:05 -0400

    Super easter predictor

--- change-machine.js
+++ change-machine.js
@@ -1,1 +1,6 @@
-// Author: FirstName LastName
+// Author: Kevin Chiriboga
+var readline = require("readline-sync");
+var number = readline.question("enter your change");
+let x = number % 25
+let y = x % 10 
+console.log(number + " makes ")
