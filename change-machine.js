dff3b582870094e1d227cc902f32c38ba917daf8
Kevin Chiriboga <kevin.chiriboga102@gmail.com>
2018-07-10 17:01:03 -0400

    Lesson 4

--- change-machine.js
+++ change-machine.js
@@ -1,6 +1,11 @@
 // Author: Kevin Chiriboga
 var readline = require("readline-sync");
-var number = readline.question("enter your change");
-let x = number % 25
-let y = x % 10 
-console.log(number + " makes ")
+let num1 = parseInt(readline.question("enter your change"));
+console.log( num1 + " makes ")
+let x = Math.floor(num1 / 25)
+console.log( x + " Quarters ")
+let y = Math.floor(num1 / 10)
+console.log( y + " Dimes ")
+let z = Math.floor(num1 / 5)
+console.log( z + " Nickles")
+//Make it less than
--- hello.js
+++ hello.js
@@ -1,4 +1,4 @@
 // Author: FirstName LastName
 var readline = require("readline-sync");
 var userName = readline.question("please enter your name");
-console.log("hello " + userName  + "!");
+console.log(" hello " + userName  + "!");
