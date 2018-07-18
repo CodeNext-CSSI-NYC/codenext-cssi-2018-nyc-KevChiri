650306ecef188089f881e0ddf9ae88d55ac87b70
Amadou Gamby <agamby23@gmail.com>
2018-07-18 12:55:36 -0400

    lesson 12 review

---
+++ scratchpad2.js
@@ -0,0 +1,12 @@
+var dialpad = [
+  ["1","2","3"],
+  ["4","5","6"],
+  ["7","8","9"],
+  ["*","0","#"]
+];
+
+for(var i = 0; i < dialpad.length;i++){
+  for(var j = 0; j < dialpad[i].length; j++){
+    console.log(dialpad[i][j]);
+  }
+}
